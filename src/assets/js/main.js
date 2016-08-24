7a4df2c378f7ab3e63ce5d19779559c5f1a07f23
Steven T Ancheta <steventancheta@gmail.com>
2016-08-24 11:29:28 -0700

    Completed Graph, Legend, and tooltip. Next - axes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -24,9 +24,12 @@
   };
 
   var handleData = function(data) {
-    var margin = {top: 200, right: 200, bottom: 70, left: 200},
-      width = 900 - margin.left - margin.right,
-      height = 900 - margin.top - margin.bottom;
+    var baseTemp = data.baseTemperature;
+    var mVariance = data.monthlyVariance;
+
+    var margin = {top: 100, right: 100, bottom: 70, left: 100},
+      width = 1350 - margin.left - margin.right,
+      height = 750 - margin.top - margin.bottom;
 
     var svg = d3.select('.chart').append('svg')
         .attr('width', width + margin.left + margin.right)
@@ -34,6 +37,78 @@
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+    mVariance.forEach(function(d) {
+      d.year = +d.year;
+      d.month = +d.month;
+      d.variance = +d.variance;
+    });
+
+    var maxYear = d3.max(mVariance, function(d) { return d.year; });
+    var minYear = d3.min(mVariance, function(d) { return d.year; });
+    var maxVariance = d3.max(mVariance, function(d) { return d.variance; });
+    var minVariance = d3.min(mVariance, function(d) { return d.variance; });
+
+    var color = d3.scale.linear().domain([0, 6, 11]).range(['#6996AD', '#FFF68F', '#660000']);
+    var colorScale = d3.scale.linear().domain([minVariance + baseTemp, maxVariance + baseTemp]).range([0,11]);
+    var roundThousand = function(num) { return Math.floor(num * 1000) / 1000};
+    var months = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October', 'November', 'December'];
+    var x = d3.scale.linear().domain([minYear, maxYear]).range([0, width / 1.3]);
+    var y = d3.scale.linear().domain([11, 0]).range([height - margin.bottom, 0]);
+
+    var tooltip = d3.select('.chart')
+                .append('div')
+                .attr('class', 'tooltip')
+                .style('opacity', 0);
+
+    var heatmap = svg.selectAll(".heatmap")
+                    .data(mVariance)
+                    .enter().append('rect')
+                    .attr('class', 'bin')
+                    .attr('x', function(d) { return (x(d.year) + ((d.year - minYear) + 1)); })
+                    .attr('y', function(d) { return y((d.month - 1)); })
+                    // .attr("transform", function(d, i) { return "translate(" + i * 5 + ", 0)"; })
+                    .attr('width', 3)
+                    .attr('height', 45)
+                    .style('fill', function(d) { return color(colorScale(d.variance + baseTemp)); })
+                    .on('mouseover', function(d) {
+                       tooltip.transition()
+                              .duration(100)
+                              .style('opacity', 0.9);
+                       tooltip.html(
+                         '<strong>' + d.year + ' - ' + months[(d.month - 1)] + '</strong><br>' +
+                         '<strong>' + roundThousand(d.variance + baseTemp) + '</strong><br> ' +
+                         d.variance
+                        )
+                        .style('left', (d3.event.pageX + 20) + 'px')
+                        .style('top', (d3.event.pageY - 20) + 'px');
+
+                     })
+                     .on('mouseout', function() {
+                       tooltip.transition()
+                              .duration(200)
+                              .style('opacity', 0);
+                     });
+
+    var legend = svg.selectAll('.legend')
+                .data(color)
+                .enter().append('g')
+                .attr('class', 'legend')
+                .attr("transform", function(d, i) { return "translate(" + i * 30 + ", 0)"; });
+
+    legend.append("rect")
+          .attr("x", width - 290)
+          .attr("y", height + 10)
+          .attr("width", 30)
+          .attr("height", 15)
+          .style("fill", function(d, i) { return color(i); });
+
+    legend.append("text")
+        .attr("x", width - 175)
+        .attr("y", 5)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d, i) { return colorScale[i]; });
     console.log(data);
   };
 
